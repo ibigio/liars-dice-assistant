0a94387c11acc48fc952323fc0ea4420eeb47204
Ilan Bigio <ilanbigio@gmail.com>
2020-10-13 22:06:39 -0700

    Hello world with nicer file structure.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../images/logo.svg';
+import Welcome from './welcome';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Welcome name="Ilan"/>
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
